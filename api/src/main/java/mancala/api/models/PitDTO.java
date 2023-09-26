7f6f26cfdb7088b89bcc5c37bd1278a41df9edb3
Jetze Zoethout <jzoethout@sogyo.nl>
2023-09-26 14:01:30 +0200

    fix: overal spaces gebruiken voor indenting

--- api/src/main/java/mancala/api/models/PitDTO.java
+++ api/src/main/java/mancala/api/models/PitDTO.java
@@ -2,27 +2,27 @@ package mancala.api.models;
 
 public class PitDTO {
 
-	private int index;
-	private int nrOfStones;
+    private int index;
+    private int nrOfStones;
 
-	public PitDTO(int index, int nrOfStones) {
-		this.index = index;
-		this.nrOfStones = nrOfStones;
-	}
+    public PitDTO(int index, int nrOfStones) {
+        this.index = index;
+        this.nrOfStones = nrOfStones;
+    }
 
-	public int getIndex() {
-		return index;
-	}
+    public int getIndex() {
+        return index;
+    }
 
-	public void setIndex(int index) {
-		this.index = index;
-	}
+    public void setIndex(int index) {
+        this.index = index;
+    }
 
-	public int getNrOfStones() {
-		return nrOfStones;
-	}
+    public int getNrOfStones() {
+        return nrOfStones;
+    }
 
-	public void setNrOfStones(int nrOfStones) {
-		this.nrOfStones = nrOfStones;
-	}
+    public void setNrOfStones(int nrOfStones) {
+        this.nrOfStones = nrOfStones;
+    }
 }
--- api/src/main/java/mancala/api/models/PlayerDTO.java
+++ api/src/main/java/mancala/api/models/PlayerDTO.java
@@ -4,33 +4,33 @@ import mancala.domain.Playable;
 
 public class PlayerDTO {
 
-	public String name;
-	public String type;
-	public boolean hasTurn;
-	public PitDTO[] pits = new PitDTO[7];
+    public String name;
+    public String type;
+    public boolean hasTurn;
+    public PitDTO[] pits = new PitDTO[7];
 
-	public PlayerDTO(
-			Playable mancala,
-			String name) {
-		this.name = name;
-		this.hasTurn = mancala.isPlayersTurn(name);
-		int firstHoleIndex = this.name == mancala.getNameOfPlayerOne() ? 0 : 7;
-		for (int i = 0; i < 7; ++i) {
-			this.pits[i] = new PitDTO(
-					firstHoleIndex + i,
-					mancala.getStonesForPit(i + firstHoleIndex));
-		}
-	}
+    public PlayerDTO(
+            Playable mancala,
+            String name) {
+        this.name = name;
+        this.hasTurn = mancala.isPlayersTurn(name);
+        int firstHoleIndex = this.name == mancala.getNameOfPlayerOne() ? 0 : 7;
+        for (int i = 0; i < 7; ++i) {
+            this.pits[i] = new PitDTO(
+                    firstHoleIndex + i,
+                    mancala.getStonesForPit(i + firstHoleIndex));
+        }
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public boolean getHasTurn() {
-		return hasTurn;
-	}
+    public boolean getHasTurn() {
+        return hasTurn;
+    }
 
-	public PitDTO[] getPits() {
-		return pits;
-	}
+    public PitDTO[] getPits() {
+        return pits;
+    }
 }
