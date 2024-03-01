a6f5521b7994708c799cf6146c42240f25bbe97b
Reinier Bakker <rbakker@sogyo.nl>
2024-03-01 13:23:03 +0100

    opmaak rematch button aangepast

--- client/src/components/RematchButton.tsx
+++ client/src/components/RematchButton.tsx
@@ -10,8 +10,8 @@ export const RematchButton = () => {
     const [alert, setAlert] = useState<string | null>(null);
     const { gameState, setGameState } = useMancalaGame();
     // const { to, text, isActive } = props;
-    const player1 = gameState.players[1].name
-    const player2 = gameState.players[0].name
+    const player1 = gameState?.players[1].name
+    const player2 = gameState?.players[0].name
 
     const onSubmit = async () => {
         const result = await startGame(player1, player2);
@@ -25,9 +25,11 @@ export const RematchButton = () => {
 
     return (<button className={classNames(
         "py-1 px-3 rounded-full text-xl border-4",
-        "bg-neutral-50",
-        "text-neutral-800 ",
-        "duration-300 hover:bg-neutral-100 hover:border-neutral-300",        // { "text-neutral-300 bg-sogyo border-neutral-300": false },
+        // "bg-sogyo",
+        // "border-green700",
+        "border-neutral-300",
+        "text-neutral-800",
+        "duration-300 hover:bg-sogyo hover:border-sogyo hover:text-neutral-100",        // { "text-neutral-300 bg-sogyo border-neutral-300": false },
         )}
         onClick={() => onSubmit()}>
         rematch
