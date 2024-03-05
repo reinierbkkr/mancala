8b88843ac555dfc935cf55825256e167232cb4ec
Reinier Bakker <rbakker@sogyo.nl>
2024-03-05 15:06:10 +0100

    average coverage build once

--- testandaveragecoverage.sh
+++ testandaveragecoverage.sh
@@ -1,8 +1,8 @@
 # Run your build and tests
-./gradlew clean test -Pversion="${CI_PIPELINE_ID}-${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
+#./gradlew clean test -Pversion="${CI_PIPELINE_ID}-${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}"
 
 # Extract all line coverage percentages from the console output and calculate average
-average_coverage=$(./gradlew test | grep -oE 'Line Coverage: ([0-9.]+)%' | grep -oE '[0-9.]+' | awk '{ total += $1 } END { print total / NR }')
+average_coverage=$(./gradlew clean build -Pversion="${CI_PIPELINE_ID}-${CI_COMMIT_BRANCH}-${CI_COMMIT_SHORT_SHA}" | grep -oE 'Line Coverage: ([0-9.]+)%' | grep -oE '[0-9.]+' | awk '{ total += $1 } END { print total / NR }')
 
 # Print the average coverage
 echo "Average Coverage: ${average_coverage}%"
